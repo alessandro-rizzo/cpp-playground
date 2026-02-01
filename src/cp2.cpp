97574d8ae5ff6aae812f56a4f45e59f4ce1216b8
Alessandro Rizzo <alessandro.rizzo@ki-insurance.com>
2026-02-01 17:27:40 +0000

    first exec and formatting

--- src/cp2.cpp
+++ src/cp2.cpp
@@ -2,11 +2,31 @@
 
 // Chapter 2: User-Defined Types exercises and examples
 
+// Exercis 1
+struct Rectangle {
+  int width;
+  int height;
+};
+
+void rectangle_init(Rectangle& r, int width, int height) {
+  r.width = width;
+  r.height = height;
+};
+
+int area(Rectangle& r) {
+  return r.height * r.width;
+};
+int perimeter(Rectangle& r) {
+  return 2 * r.height + 2 * r.width;
+};
+
 int main() {
-    std::cout << "Chapter 2: User-Defined Types\n";
-    std::cout << "===============================\n\n";
+  std::cout << "Chapter 2: User-Defined Types\n";
+  std::cout << "===============================\n\n";
 
-    // Your code here
-
-    return 0;
+  Rectangle r;
+  rectangle_init(r, 2, 3);
+  std::cout << area(r) << '\n';
+  std::cout << perimeter(r) << '\n';
+  m return 0;
 }
