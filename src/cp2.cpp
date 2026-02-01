509886a4ddf7f93054e08732664bca1d433979dd
Alessandro Rizzo <alessandro.rizzo@ki-insurance.com>
2026-02-01 17:48:03 +0000

    more exec

--- src/cp2.cpp
+++ src/cp2.cpp
@@ -20,6 +20,44 @@ int perimeter(Rectangle& r) {
   return 2 * r.height + 2 * r.width;
 };
 
+class RRectangle {
+public:
+  RRectangle(int height, int width) : h{height}, w{width} {};
+  int area() {
+    return h * w;
+  };
+  int perimeter() {
+    return 2 * w + 2 * h;
+  };
+  bool square() {
+    return h == w;
+  };
+
+private:
+  int h;
+  int w;
+};
+
+class Complex {
+public:
+  Complex(float real, float imaginary) : r{real}, i{imaginary} {};
+  Complex operator+(Complex& o) {
+    return Complex(r + o.r, i + o.i);
+  };
+  void print() const {
+    cout << real;
+    if (imag >= 0)
+      cout << " + " << imag << "i";
+    else
+      cout << " - " << -imag << "i";
+    cout << endl;
+  }
+
+private:
+  float r;
+  float i;
+};
+
 int main() {
   std::cout << "Chapter 2: User-Defined Types\n";
   std::cout << "===============================\n\n";
@@ -28,5 +66,11 @@ int main() {
   rectangle_init(r, 2, 3);
   std::cout << area(r) << '\n';
   std::cout << perimeter(r) << '\n';
-  m return 0;
+
+  RRectangle rr(2, 3);
+  std::cout << rr.area() << '\n';
+  std::cout << rr.perimeter() << '\n';
+  std::cout << rr.square();
+
+    return 0;
 }
